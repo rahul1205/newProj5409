d072f41b6b2cc7ba99226afe09f46e13548a430c
Rahul Anand <anand.rahul1205@gmail.com>
2020-02-29 01:12:36 +0530

    User2: fixed bugs fibonnaci.py

--- fibonnaci.py
+++ fibonnaci.py
@@ -3,32 +3,36 @@
 import time
 import matplotlib.pyplot as plt
 
-def fibonacci(number):  
-    if number==1: 
-        return 0
-    elif number==2: 
-        return 1
-    else: 
-        return fibonacci(number-1)+fibonacci(number-2) 
+def fibonacci(number): 
+    if number <= 1:
+       return number
+    else:
+       return(fibonacci(number-1) + fibonacci(number-2))
+
+# output_str =    ""
+# for i in range(int(5)):
+
+#     output_str += str(fibonacci(i)) + "|"
+# print (output_str)
+
+
 try:
     file = open('input_cases.txt', 'r')
 except:
     pass
-
-x_axis_value = []
-y_axis_value = []
+output = open('fibonnaci_output.csv', 'w+')
+output.write("RequestID,Time,Number,Result\n")
 if file:
     numbers = file.read().splitlines()
     for i, x in enumerate(numbers):
         # unique = uuid.uuid4()
         start = time.time()
-        fibonacci(int(x))
+        output_str =    ""
+        for i in range(int(x)):
+            output_str += str(fibonacci(i)) + "#"
+        print (output_str.rstrip("#"))
         time_taken = time.time() - start
-        y_axis_value.append(time_taken)
-        x_axis_value.append(str(i))
-# print (y_axis_value)
-plt.xticks([x*10 for x in range(0,10)])
-plt.xlabel("Request ID")
-plt.ylabel("Time")
-plt.plot(x_axis_value, y_axis_value, color='red')
-plt.savefig("fibonacci.png")
+        to_write = str(i) + "," + str(time_taken) + "," + str(x) + "," + str(output_str) + "\n"
+        output.write(to_write)
+
+
