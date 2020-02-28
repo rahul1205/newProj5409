031c588748da8f552c808ce3ea4af94ae915b1e5
Rahul Anand <anand.rahul1205@gmail.com>
2020-02-29 00:47:00 +0530

    User1: update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,7 +1,6 @@
 
 # import uuid
 import time
-import matplotlib.pyplot as plt
 
 def factorial(number):
     if number == 1:
@@ -12,21 +11,16 @@ try:
     file = open('input_cases.txt', 'r')
 except:
     pass
+output = open('factorial_output', 'w+')
+output.write("RequestID,Time,Number,Result\n")
 
-x_axis_value = []
-y_axis_value = []
 if file:
     numbers = file.read().splitlines()
     for i, x in enumerate(numbers):
         # unique = uuid.uuid4()
         start = time.time()
-        factorial(int(x))
+        result = factorial(int(x))
         time_taken = time.time() - start
-        y_axis_value.append(time_taken)
-        x_axis_value.append(str(i))
+        to_write = str(i) + "," str(time_taken) + "," + str(x) + "," + str(result) + "\n"
+        output.write(to_write)
 # print (y_axis_value)
-plt.xticks([x*10 for x in range(0,10)])
-plt.xlabel("Request ID")
-plt.ylabel("Time")
-plt.plot(x_axis_value, y_axis_value, color='red')
-plt.savefig("factorial.png")
